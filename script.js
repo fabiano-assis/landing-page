06daf589e31c522d124364928fe8100cc32457cd
Fabiano Assis <fabianoa602@gmail.com>
2025-09-25 20:38:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+        const modal = document.getElementById("modalTratamentos");
+        const btnTratamentos = document.querySelector(".links a.btn"); // primeiro botão
+        const spanClose = document.querySelector(".close");
+
+        btnTratamentos.addEventListener("click", function(e) {
+            e.preventDefault(); // evita abrir link
+            modal.style.display = "block";
+        });
+
+        spanClose.addEventListener("click", function() {
+            modal.style.display = "none";
+        });
+
+        window.addEventListener("click", function(e) {
+            if (e.target === modal) {
+            modal.style.display = "none";
+            }
+        });
+    </script>
